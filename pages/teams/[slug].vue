9ad865c9943c7c118430893656babd89de2b7a05
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-05-03 19:49:38 +0200

    scaffold dynamic pages for teams and players

--- pages/teams/[slug].vue
+++ pages/teams/[slug].vue
@@ -1,9 +1,8 @@
 <template>
-  <div></div>
+  <section class="page-section">
+    <h2 class="section-title">Coming Soon</h2>
+    <p class="text-gray-600">
+      This is a placeholder for the {{ $route.params.slug }} page.
+    </p>
+  </section>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped></style>
