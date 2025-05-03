ecb61c04759df1d3789e9520d4ae4a1b9f6bad07
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-05-03 19:48:46 +0200

    Add placeholder standings page. Basic empty templates to other pages

---
+++ pages/standings.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="page-section">
+    <h2 class="section-title">League Standings</h2>
+    <p class="text-gray-600">
+      Standings table will be loaded here from Umbraco.
+    </p>
+  </section>
+</template>
