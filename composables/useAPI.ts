98a86b913169fc0717eb1938d95bcd6899cf13c8
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2025-05-10 19:50:43 +0200

    Removed console logs

--- composables/useAPI.ts
+++ composables/useAPI.ts
@@ -3,15 +3,13 @@ export const useApi = () => {
 
   const get = async (endpoint: string) => {
     const url = `${baseUrl}${endpoint}`;
-    console.log('Fetching (raw):', url);
 
     try {
       const res = await fetch(url);
       const json = await res.json();
-      console.log('Raw fetch success:', json);
+
       return json;
     } catch (err) {
-      console.error('Raw fetch failed:', err);
       return null;
     }
   };
